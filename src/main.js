c0f399e9f0f4a3f2ce07e361192db271b1c60f44
MrBladez <austin.minute@gmail.com>
2025-04-23 09:18:44 -0500

    4/23 app download

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
+
+// Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+  })
+}
